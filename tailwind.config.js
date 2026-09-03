13a39d74fa4b37e8751f8ede0f27a763a4e95b87
Md Towfiqur Rahman <140777602+towfiqurv360@users.noreply.github.com>
2026-09-03 15:05:35 +0600

    style: update tailwind configuration for theme support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class", // ডার্ক মোডের জন্য এটি অত্যন্ত জরুরি
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
